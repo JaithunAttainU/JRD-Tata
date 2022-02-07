9fd19a1e65b88fbde801d92c523fa192a4b43b21
JaithunMahira <jaithun.mahira@attainu.com>
2022-02-08 00:09:29 +0530

    Dev Tools & Introduction to JavaScript

---
+++ Css/Revision/Dev Tools/script.js
@@ -0,0 +1,14 @@
+function onbuttonClickHander() {
+  console.log("Button Clicked")
+  func2("hello");
+  alert("Hi")
+}
+
+function func2(val) {
+  var name = "xyz"
+  console.log(val);
+}
+
+// func2 (script.js:9)
+// onbuttonClickHander (script.js:3)
+// onclick (index.html:22)
---
+++ JavaScript/Day 1/myFirstScript.js
@@ -0,0 +1,17 @@
+
+var studentsName = "xyz"
+
+studentsName = true;
+function myFirstFucntion() {
+  var name = undefined;
+  var age = 10;
+  var studentsName = "abc"
+
+  console.log("Inside Function", studentsName)  //print statement in console
+}
+
+myFirstFucntion()
+console.log(studentsName)
+// console.log("Outside Function" + age)
+
+// ES6: let, const
