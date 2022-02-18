f5d1298d48e82e1c79c5adbe0331980cb82070c3
JaithunMahira <jaithun.mahira@attainu.com>
2022-02-18 20:00:32 +0530

    Hoisting & Closures

---
+++ JavaScript/Day 9/script.js
@@ -0,0 +1,100 @@
+
+//Hoisting
+
+var result = add(5, 4)
+
+console.log(studentName) //undefined
+var studentName = 'Sitanshu';
+
+
+function add(num1, num2) {
+
+  console.log(studentAge);
+
+  var studentAge = 25;
+  function doSomething() {
+
+  }
+  return num1 + num2
+}
+
+// appears to be ->
+
+var studentName;
+var result;
+function add(num1, num2) {
+  return num1 + num2
+}
+
+console.log(studentName)
+studentName = 'Sitanshu';
+result = add(5, 4)
+
+
+
+//Closures
+var count = 0;
+function addCount() {
+  count++;
+}
+
+
+
+
+function initCart() {
+  var totalPrice = 0;
+
+  var kdskdlc = 100;
+
+  function addProductPrice(amount) {
+    totalPrice += amount;
+    console.log(kdskdlc)
+    var addVariable = 10
+    function doSomething() {
+      console.log(addVariable);
+      console.log(totalPrice);
+    }
+
+    return doSomething
+  }
+
+  function removeProductPrice(amount) {
+    totalPrice -= amount;
+  }
+
+  function getPrice() {
+    return totalPrice;
+  }
+
+  return {
+    addPrice: addProductPrice,
+    removePrice: removeProductPrice,
+    getPrice: getPrice
+  };
+}
+
+var cartObj = initCart();
+
+var doSomething = cartObj.addPrice(10);
+// addProductPrice(5)
+
+
+//Example
+var a = 1;
+function b() {
+  console.log(a);
+  a = 10;
+  return;
+  function a() {
+    console.log("doSomething")
+  }
+}
+
+b();
+console.log(a);
+
+//1 10
+
+//10 1
+
+//1 
