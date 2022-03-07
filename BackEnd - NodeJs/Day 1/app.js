0c15786f89da33c243bae8c199e3f88a5e280082
JaithunMahira <jaithun.mahira@attainu.com>
2022-03-07 23:41:51 +0530

    BackEnd - Intro to Node, NPM

---
+++ BackEnd - NodeJs/Day 1/app.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk') // commonJS Modules
+
+// import chalk from 'chalk'; //ES6 Modules
+console.log(chalk.blue("Hello Node!"))
+
+// document.getElementById('task')
+// localStorage.clear()
+
+setTimeout(() => console.log("Timer"), 2000)
