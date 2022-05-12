44d59a25016cb9e5275f007200660c649d24a920
JaithunMahira <jaithun.mahira@attainu.com>
2022-05-12 20:05:16 +0530

    React states

---
+++ React/React - Day4/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+import CartCounter from './components/CartCounter';
+
+function App() {
+
+  return (
+    <CartCounter />
+  )
+}
+
+export default App;
---
+++ React/React - Day4/src/mock/images.js
@@ -0,0 +1,9 @@
+const images = [
+  "https://media.istockphoto.com/photos/anti-inflammanory-diet-food-picture-id1299079241",
+  "https://media.istockphoto.com/photos/food-for-healthy-brain-picture-id1299079243",
+  "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38",
+  "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe",
+  "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445",
+  "https://images.unsplash.com/photo-1565958011703-44f9829ba187"
+
+]
