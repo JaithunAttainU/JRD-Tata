7ed1d02abe7126df5e76622ef0249b8152b7e6a0
JaithunMahira <jaithun.mahira@attainu.com>
2022-06-07 15:49:55 +0530

    Debouncing, useRef

--- React/React - Day1/src/index.js
+++ React/React - Day1/src/index.js
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-
+// import { throttle } from 'lodash'
 function App() {
   //hooks
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const countRef = useRef(0)
+  console.log(countRef)
 
+  //TODO: Check
+  // countRef.data = 'fserf'
+  console.log("Component Rerendered")
   return (
     <div>
       <button onClick={() => {
-        setCount(count + 1)
-      }}>+</button>
-      <div><p>Count is <span>{count}</span></p></div>
-      <button onClick={() => {
-        setCount(count - 1)
-      }}>-</button>
+        // setCount(count + 1) //async
+        countRef.current = countRef.current + 1 //synchronous
+        console.log(countRef.current)
+
+      }}>ClickMe{countRef.current}</button>
     </div>
   );
 }
--- React/React - Day17/src/components/SearchBar/index.js
+++ React/React - Day17/src/components/SearchBar/index.js
@@ -1,14 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
 import { useStyles } from './style';
 import { useDispatch } from 'react-redux';
 import { videosAction } from '../../actions/videos';
+import { debounce } from '@material-ui/core';
+import { useRef } from 'react';
 
 export default function SearchBar() {
   const classes = useStyles();
   const dispatch = useDispatch()
   const [searchTerm, setSearchTerm] = useState('')
+
+  const inputEleRef = useRef()
+  const materialInputRef = React.createRef()
+
+  const debounce = (callback, delay) => {
+    let timerId;
+    return function (searchTerm) {
+      clearTimeout(timerId)
+      timerId = setTimeout(() => {
+        callback(searchTerm)
+      }, delay)
+    }
+  }
+
+
+  useEffect(() => {
+    // console.log(document.getElementById('inputText'))
+    // document.getElementById('inputText').focus()
+    inputEleRef.current.focus()
+    // inputEleRef.current.style.backgroundColor = 'green'
+
+    materialInputRef.current.children[0].focus()
+    // console.log()
+  }, [])
+
+
+  // let timerId;
+  // const debounce = (delay, searchTerm) {
+  //   clearTimeout(timerId)
+  //   timerId = setTimeout(() => {
+  //     setSearchTerm(searchTerm)
+  //     dispatch(videosAction(searchTerm))
+  //   }, 1000)
+  // }
+
+
+  const update = debounce((searchTerm) => {
+    setSearchTerm(searchTerm)
+    dispatch(videosAction(searchTerm))
+  }, 1000)
+
 
   return (
     <div className={classes.search}>
@@ -21,14 +64,35 @@ export default function SearchBar() {
           root: classes.inputRoot,
           input: classes.inputInput,
         }}
-        value={searchTerm}
+        ref={materialInputRef}
         onChange={(e) => {
-          //async
-          setSearchTerm(e.target.value)
-          dispatch(videosAction(e.target.value))
+          update(e.target.value)
+          // debounce(1000, e.target.value)
         }}
         inputProps={{ 'aria-label': 'search' }}
       />
+      <input type={'text'} placeholder="Enter here.." id="inputText" ref={inputEleRef} />
     </div>
   )
 }
+
+
+// i) Debouncing
+// ii) Throttling
+
+
+// h - api Call - h
+// e - api call - he
+// l -api call - hel
+
+// h - Timer, 1000 - clearTimer
+// e - Timer, 1000 - clearTimer
+// l - Timer, 1000 - clearTimer
+// l - Timer,1000 - then a api call is made
+
+
+// Two seconds - api 
+// Two seconds - api
+
+// 1 - 5 - api hello bed cat
+// 5 - 10 - api
